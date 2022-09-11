7ff8fdc29d34856a7998c71c172f3f70a177d81b
danielsear <danielandradeseara@hotmail.com>
2022-09-11 12:55:16 -0300

    list

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express'
 import { roomRepository } from '../repositories/RoomRepository'
+import { subjectRepository } from '../repositories/SubjectRepository'
 import { videoRepository } from '../repositories/VideoRepository'
 
 
@@ -53,4 +54,55 @@ export class RoomController{
        
     }  
    }
+
+   async RoomSubject (req : Request, res: Response){
+    const {subject_id} = req.body
+    const {idRoom} = req.params
+
+    try {
+      
+      const room = await roomRepository.findOneBy({id: Number(idRoom)})
+      if(!room){
+         return res.status(404).json({message: 'Aula não existe.'})
+      }
+
+      const subject = await subjectRepository.findOneBy({id: Number(subject_id)})
+      if(!subject){
+        return res.status(404).json({message: 'Disciplina não existe.'})
+     }
+
+     const roomUpdate = {
+      ...room,
+        subjects: [subject]
+     }
+      await roomRepository.save(roomUpdate)
+      return res.status(204).send()
+
+    } catch (error) {
+      
+      console.log(error)
+      return res.status(500).json('Internal Server Error.')
+
+    }
+   }
+
+   async List (req : Request, res: Response){
+
+      try {
+        const rooms = await roomRepository.find({
+          relations: {
+            subjects: true
+          }
+        })
+        
+        return res.json(rooms)
+
+      } catch (error) {
+        
+        console.log(error)
+        return res.status(500).json('Internal Server Error.')
+  
+      }
+   }
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,10 @@ const routes = Router()
 routes.post('/subject', new SubjectController().Create)
 routes.post('/room', new RoomController().Create)
 routes.post('/room/:idRoom/create',new RoomController().CreateVideo)
+routes.post('/room/:idRoom/subject',new RoomController().RoomSubject)
+routes.get('/room',new RoomController().List)
+
+
 
 
 
