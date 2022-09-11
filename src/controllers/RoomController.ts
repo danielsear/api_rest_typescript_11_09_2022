b30f7ee28fdf4d5d8369159f5b7340e51351627f
danielsear <danielandradeseara@hotmail.com>
2022-09-11 12:24:22 -0300

    created the class video attachment method.

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express'
 import { roomRepository } from '../repositories/RoomRepository'
+import { videoRepository } from '../repositories/VideoRepository'
 
 
 
@@ -18,10 +19,38 @@ export class RoomController{
       return res.status(201).json(newRoom)
 
    } catch (error) {
-    
+
       console.log(error)
       return res.status(500).json('Internal Server Error.')
       
    }  
   }
+
+  async CreateVideo (req : Request, res: Response){
+    const {title , url} = req.body
+    const {idRoom} = req.params
+
+    try {
+      //const room = roomRepository.findOne({where : {id: Number(idRoom)}})
+        const room = await roomRepository.findOneBy({id: Number(idRoom)})
+
+        if(!room){
+           return res.status(404).json({message: 'Aula não existe.'})
+        }
+      
+        const newVideo = videoRepository.create({
+          title,
+          url,
+          room
+        })
+       await videoRepository.save(newVideo)
+       return res.status(201).json(newVideo)
+ 
+    } catch (error) {
+ 
+       console.log(error)
+       return res.status(500).json('Internal Server Error.')
+       
+    }  
+   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,15 @@ import {Router} from 'express'
 import { RoomController } from './controllers/RoomController'
 import { SubjectController } from './controllers/SubjectController'
 
+
+
+
 const routes = Router()
 
 routes.post('/subject', new SubjectController().Create)
 routes.post('/room', new RoomController().Create)
+routes.post('/room/:idRoom/create',new RoomController().CreateVideo)
+
 
 
 export default routes
